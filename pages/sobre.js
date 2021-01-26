eb88dd09f5a40be9b6e11d9c7b8e463dab68933f
BrenerViotto <brenerviotto@gmail.com>
2021-01-26 12:08:47 -0300

    teste api

--- pages/sobre.js
+++ pages/sobre.js
@@ -10,11 +10,18 @@ function Sobre() {
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             </Head>
             <h1>Sobre</h1>
+            <p>{stars}</p>
         <Link href="/">
             <a>Home</a>
         </Link>
         </div>
     )
 }
+
+Page.getInitialProps = async (ctx) => {
+    const res = await fetch('https://api.github.com/repos/vercel/next.js')
+    const json = await res.json()
+    return { stars: json.stargazers_count }
+  }
 
 export default Sobre
