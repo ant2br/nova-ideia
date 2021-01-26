afde903f2c00cd23af79ca69a2c8101c21f23ac6
BrenerViotto <brenerviotto@gmail.com>
2021-01-26 12:00:44 -0300

    title

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import 'next/link';
 import Link from 'next/link';
-
+import Head from 'next/head';
 function Home() {
     return (
         <div>
+            <Head>
+                <title>Sobre</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
             <h1>Home</h1>
         <Link href="/sobre">
             <a>Sobre</a>
--- pages/sobre.js
+++ pages/sobre.js
@@ -1,9 +1,14 @@
 import 'next/link';
 import Link from 'next/link';
+import Head from 'next/head';
 
 function Sobre() {
     return (
         <div>
+            <Head>
+                <title>Sobre</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
             <h1>Sobre</h1>
         <Link href="/">
             <a>Home</a>
