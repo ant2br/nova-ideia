cfbede54fb60675a2d0e533adb81739e833ca24c
BrenerViotto <brenerviotto@gmail.com>
2021-01-26 09:42:45 -0300

    alteração

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,15 +1,15 @@
 import 'next/link';
 import Link from 'next/link';
 
-function Home() {
+function Sobre() {
     return (
         <div>
-            <h1>Home</h1>
-        <Link href="/sobre">
-            <a>Sobre</a>
+            <h1>Sobre</h1>
+        <Link href="/">
+            <a>Home</a>
         </Link>
         </div>
     )
 }
 
-export default Home
+export default Sobre
